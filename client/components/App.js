1650b373b242a0b7a5fe52a944ec7db297b250b9
Cássio Zen <cassiozen@gmail.com>
2018-05-18 10:17:13 -0400

    Solution

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import AddGrocery from "./AddGrocery";
+import Footer from "./Footer";
+import GroceryList from "./GroceryList";
 
 const App = () => (
   <div className="app">
-    <h1>Hello World</h1>
+    <img src="groceries.png" alt="Groceries" width="500" />
+
+    <div className="list">
+      <AddGrocery />
+      <GroceryList groceries={[]} />
+      <Footer />
+    </div>
   </div>
 );
 
